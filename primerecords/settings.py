c6f43094b594a2969b6f429eadc62b3f892919e0
unknown <nnadidan360@gmail.com>
2021-11-01 23:49:42 +0100

    changed stuff

--- primerecords/settings.py
+++ primerecords/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = ')fun-0cq$quopm&ycnxjor_xnb3j87ux%hll#ug!7e)jorg&et'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
