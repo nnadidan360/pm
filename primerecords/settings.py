c5454c895d2e8578f805ebdb96906b969cc6f8b0
unknown <nnadidan360@gmail.com>
2021-11-02 00:02:12 +0100

    changed stuff

--- primerecords/settings.py
+++ primerecords/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = ')fun-0cq$quopm&ycnxjor_xnb3j87ux%hll#ug!7e)jorg&et'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -153,13 +153,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'primerecords/static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR, 'primerecords/static')
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'primerecords/static'),
-# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'primerecords/static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'primerecords/static'),
+)
